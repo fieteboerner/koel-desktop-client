92d1de0f5457dc7affb95b20eaa8a95b75e17bbe
Fiete Börner <fieteboerner@googlemail.com>
2018-02-11 19:10:00 +0100

    add song, album and artist object definitions

---
+++ src/renderer/stubs/album.js
@@ -0,0 +1,10 @@
+import artist from './artist'
+
+export default {
+  id: 0,
+  name: '',
+  cover: '',
+  artist_id: 0,
+  artist,
+  songs: []
+}
---
+++ src/renderer/stubs/song.js
@@ -0,0 +1,13 @@
+import album from './album'
+import artist from './artist'
+
+export default {
+  id: null,
+  title: '',
+  length: 0,
+  disc: 1,
+  album_id: 0,
+  album,
+  artist_id: 0,
+  artist
+}
