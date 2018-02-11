a6220b80a21ad68d422341baf8a4f25551231c52
Fiete Börner <fieteboerner@googlemail.com>
2018-02-11 19:40:40 +0100

    fix sidebar layout styling

--- src/renderer/components/layout/Sidebar.vue
+++ src/renderer/components/layout/Sidebar.vue
@@ -3,9 +3,9 @@
       <div class="sidebar" :style="{width}">
         <slot name="sidebar"></slot>
       </div>
-      <single-layout>
+      <div class="layout-content">
           <slot></slot>
-      </single-layout>
+      </div>
   </div>
 </template>
 <script>
@@ -13,21 +13,28 @@ export default {
   props: {
     width: {
       type: String,
-      default () { return '350px' }
+      default () {
+        return '350px'
+      }
     }
   }
 }
 </script>
-<style>
+<style lang="scss">
 .sidebar-wrapper {
   flex: 1;
   display: flex;
   flex-direction: row;
   align-content: stretch;
-}
 
-.sidebar-wrapper .sidebar {
+  .sidebar {
     overflow-y: auto;
+  }
+
+  .layout-content {
+    flex: 1;
+    overflow-y: auto;
+  }
 }
 </style>
 
