335dbb618b85eb17c98b7e5d05109693589b0be4
Fiete Börner <fieteboerner@googlemail.com>
2018-02-11 11:44:29 +0100

    add sass support

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -36,6 +36,23 @@ let mainConfig = {
       {
         test: /\.node$/,
         use: 'node-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
       }
     ]
   },
