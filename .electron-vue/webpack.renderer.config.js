8d1da3c5e29a714156df72d4c204e78e99b362fd
Fiete Börner <fieteboerner@googlemail.com>
2018-10-13 20:49:31 +0200

    remove eslint from webpack config

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -38,17 +38,6 @@ let rendererConfig = {
           appendTsSuffixTo: [/\.vue$/]
         }
       },
-      // {
-      //   test: /\.(js|vue)$/,
-      //   enforce: 'pre',
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'eslint-loader',
-      //     options: {
-      //       formatter: require('eslint-friendly-formatter')
-      //     }
-      //   }
-      // },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
--- .electron-vue/webpack.web.config.js
+++ .electron-vue/webpack.web.config.js
@@ -25,17 +25,6 @@ let webConfig = {
           appendTsSuffixTo: [/\.vue$/]
         }
       },
-      // {
-      //   test: /\.(js|vue)$/,
-      //   enforce: 'pre',
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'eslint-loader',
-      //     options: {
-      //       formatter: require('eslint-friendly-formatter')
-      //     }
-      //   }
-      // },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
