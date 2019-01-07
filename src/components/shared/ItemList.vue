103628911a4f1bd4a35aed5fc8bcafd907f6ad88
Fiete Börner <fieteboerner@googlemail.com>
2019-01-07 10:21:33 +0100

    implement multiselection into item list

--- src/components/shared/ItemList.vue
+++ src/components/shared/ItemList.vue
@@ -7,16 +7,12 @@
       <tbody>
         <tr
           class="item-list-row"
-          :class="{ 'is-selected': selectedItem === item }"
+          :class="{ 'is-selected': isSelected(item) }"
           v-for="item in items"
           :key="item.id"
-          @click="onSelect(item)"
-          >
-          <ItemListCell
-            v-for="column in columns"
-            :column="column"
-            :item="item"
-            :key="column.id" />
+          @click="onSelect($event, item)"
+        >
+          <ItemListCell v-for="column in columns" :column="column" :item="item" :key="column.id"/>
         </tr>
       </tbody>
     </table>
@@ -28,6 +24,7 @@
 </template>
 
 <script lang="ts">
+import { indexOf, last, sortBy, without } from "lodash";
 import Vue, { CreateElement, VNode } from "vue";
 import { Component, Prop, Watch } from "vue-property-decorator";
 import ItemListColumn from "@/components/shared/ItemListColumn.vue";
@@ -39,37 +36,74 @@ import ItemListCell from "@/components/shared/ItemListCell.ts";
   }
 })
 export default class ItemList extends Vue {
-  @Prop(Array) items
-  @Prop(Boolean) selectable
-  @Prop(Object) selected
+  @Prop(Array) items;
+  @Prop(Boolean) selectable;
+  @Prop(Boolean) multiselectable;
+  @Prop([Object, Array]) selected;
 
-  columns: ItemListColumn[] = []
-  selectedItem: any = null
+  columns: ItemListColumn[] = [];
+  selectedItems: Array<any> = [];
 
   mounted() {
     this.columns = this.$slots.default
       .filter((column: VNode) => column.componentInstance)
-      .map(column => <ItemListColumn>column.componentInstance)
-
+      .map(column => <ItemListColumn>column.componentInstance);
   }
 
-  onSelect(item: any): void {
-    if(!this.selectable) return
-    if(this.selectedItem === item) return
-
-    this.selectedItem = item
-    this.$emit('selection-change', item)
+  get sortedSelected() {
+    return this.items.filter(item => this.isSelected(item));
   }
 
-  @Watch('selected', {immediate: true})
-  onSelectedChange(selected){
-    this.selectedItem = selected
+  isSelected(item) {
+    return this.selectedItems.includes(item);
+  }
+
+  onSelect(event: MouseEvent, item: any): void {
+    if (!this.selectable && !this.multiselectable) return;
+
+    // single select
+    if (!this.multiselectable) {
+      if (this.isSelected(item)) return;
+      this.selectedItems = [item];
+      this.$emit("selection-change", item);
+    }
+
+    // multi select
+    // toggle single item with ctrl or cmd
+    if (event.ctrlKey || event.metaKey) {
+      if (this.isSelected(item)) {
+        this.selectedItems = without(this.selectedItems, item);
+      } else {
+        this.selectedItems.push(item);
+      }
+      // select from to with shift
+    } else if (this.selectedItems.length && event.shiftKey) {
+      let indexes = sortBy([
+        indexOf(this.items, last(this.selectedItems)),
+        indexOf(this.items, item)
+      ]);
+
+      for (let i = indexes[0]; i <= indexes[1]; i++) {
+        if (this.isSelected(this.items[i])) continue;
+        this.selectedItems.push(this.items[i]);
+      }
+      // set item if no button is hold
+    } else {
+      this.selectedItems = [item];
+    }
+
+    this.$emit("selection-change", item, this.sortedSelected);
+  }
+
+  @Watch("selected", { immediate: true })
+  onSelectedChange(selected) {
+    this.selectedItems = Array.isArray(selected) ? selected : [selected];
   }
 }
 </script>
 
 <style lang="scss">
-@import '../../styles/settings';
+@import "../../styles/settings";
 
 .item-list {
   width: 100%;
