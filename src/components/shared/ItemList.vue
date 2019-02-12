f630092f8577dfef64ee725397ef77a3437e647f
Fiete Börner <fieteboerner@googlemail.com>
2019-02-12 22:23:40 +0100

    add base draggable/droppable events into ItemList

--- src/components/shared/ItemList.vue
+++ src/components/shared/ItemList.vue
@@ -15,22 +15,34 @@
           slot-scope="{ item }"
           :class="itemClasses(item)"
           class="item-list-item"
+          draggable="true"
           @click="onSelectItem($event, item)"
           @click.right="$emit('context', $event, item)"
           @dblclick="$emit('open', $event, item)"
+          @dragstart="onDragStart($event, item)"
+          @dragenter.prevent="onDragEnter"
+          @dragover.prevent
+          @dragleave="onDragLeave"
+          @drop.prevent="onDrop"
         >
           <slot v-bind="item"></slot>
         </div>
       </RecycleScroller>
       <div v-else class="item-list">
         <div
-          class="item-list-item"
-          :class="itemClasses(item)"
           v-for="item in items"
+          :class="itemClasses(item)"
           :key="item[keyField]"
+          class="item-list-item"
+          draggable="true"
           @click="onSelectItem($event, item)"
           @click.right="$emit('context', $event, item)"
           @dblclick="$emit('open', $event, item)"
+          @dragstart="onDragStart($event, item)"
+          @dragenter.prevent="onDragEnter"
+          @dragover.prevent
+          @dragleave="onDragLeave"
+          @drop.prevent="onDrop"
         >
           <slot v-bind="item"/>
         </div>
@@ -56,10 +68,11 @@ export default class ItemList extends Vue {
     context.dynamic = true
     return context
   }}) selectionContext: SelectionContext<any>
-  @Prop(String) itemClass: string;
-  @Prop(Boolean) virtualScroll: Boolean;
-  @Prop({ type: String, default: "id" }) keyField: string;
-  @Prop(Number) itemHeight: Number;
+  @Prop(String) itemClass: string
+  @Prop(Boolean) virtualScroll: Boolean
+  @Prop({ type: String, default: "id" }) keyField: string
+  @Prop(Number) itemHeight: Number
+  @Prop(Boolean) allowSongReordering: Boolean
 
   itemClasses(item: any) {
     const classes: Object = {
@@ -81,8 +94,44 @@ export default class ItemList extends Vue {
     this.selectionContext.selectItem(event, item)
     this.$emit('select', event, item)
   }
+
+  onDragStart(event: DragEvent, item) {
+    if (!this.selectionContext.isSelected(item)) {
+      this.selectionContext.selected = item
+    }
+    event.dataTransfer.effectAllowed = 'move'
+    event.dataTransfer.dropEffect = 'move'
+
+    const songIds = this.selectionContext.selected.map(item => item[this.keyField])
+    event.dataTransfer.setData('application/x-koel.text+plain', JSON.stringify(songIds))
+    // set Image (ghost)
+  }
+
+  onDrop(event: DragEvent) {
+    if (!this.allowSongReordering) {
+      return
+    }
+
+    this.onDragLeave(event)
+
+  }
+
+  onDragEnter(event: DragEvent) {
+    if (!this.allowSongReordering) {
+      return
+    }
+
+    const item: HTMLElement = event.target.parentNode
+    item.classList.add('drag-over')
+  }
+
+  onDragLeave(event: DragEvent) {
+    const item: HTMLElement = event.target.parentNode
+    item.classList.remove('drag-over')
+  }
+
   @Watch('items', { immediate: true })
-  onItemChange(items) {
+  onItemsChange(items) {
     if(this.selectionContext.dynamic) {
       this.$set(this.selectionContext, 'items', items);
     }
@@ -108,6 +157,10 @@ export default class ItemList extends Vue {
     overflow: hidden;
     line-height: 1.75em;
     cursor: default;
+
+    &.drag-over {
+      border-bottom: 1px solid $primary;
+    }
 
     &.is-selected {
       background-color: $cyan;
